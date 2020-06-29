e26c32e4661978ba3939abbe00d9f9a0c6663680
NicoRB28 <nicolasrodriguezboschi@gmail.com>
2020-06-29 12:20:11 -0300

    se considera string vacio y no se cuentan palabras repetidas

--- app.rb
+++ app.rb
@@ -12,12 +12,12 @@ post('/') do
     words = sentence.gsub(/[,.]/,'').split(" ")
     magicWords = []
     words.each do |word|
-        if word.length > 1
+        if word.length > 1 && !magicWords.include?(word)
             magicWords << word if word == word.reverse
         end
     end
     status = 200
     headers = {"Content-Type" => "text/html"}
-    body = ["<h1>Cantidad de palabras magicas detectadas: #{magicWords.length}</h1>"]
+    body = ["<h1>Cantidad de palabras magicas detectadas: #{(magicWords.to_set).length}</h1>"]
     [status,headers,body]
 end
